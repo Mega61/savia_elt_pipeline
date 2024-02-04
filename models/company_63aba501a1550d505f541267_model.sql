df81171c2577a589ae467b53ed3731f274aeeebe
Juan E Daza <jdazap@choucairtesting.com>
2024-02-04 15:34:41 -0500

    Added paying costumers models

--- models/company_63aba501a1550d505f541267_model.sql
+++ models/company_63aba501a1550d505f541267_model.sql
@@ -6,9 +6,9 @@
 }}
 
     
-select _id, company_id, invoice_id, total_units, status, updated_at
-from {{source('mongo_savia_core_qa', 'order')}}
+select _id, company_id, invoice_id, total_units, status
+from {{ref("customer_segregation_model")}}
+where company_id = "63aba501a1550d505f541267"
 {% if is_incremental() %}
     where updated_at > (select max(updated_at) from {{ this }})
 {% endif %}
-
--- models/company_6541bad8c8f08b67c561501b_model.sql
+++ models/company_6541bad8c8f08b67c561501b_model.sql
@@ -6,9 +6,9 @@
 }}
 
     
-select _id, company_id, invoice_id, total_units, status, updated_at
-from {{source('mongo_savia_core_qa', 'order')}}
+select _id, company_id, invoice_id, total_units, status
+from {{ref("customer_segregation_model")}}
+where company_id = "6541bad8c8f08b67c561501b"
 {% if is_incremental() %}
     where updated_at > (select max(updated_at) from {{ this }})
 {% endif %}
-
