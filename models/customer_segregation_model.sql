6cf3292813ba830f4fc828f8671258f87ecff05e
Juan E Daza <jdazap@choucairtesting.com>
2024-02-04 11:29:15 -0500

    Modified base models

---
+++ models/customer_segregation_model.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        materialized="incremental",
+        unique_key="_id",
+    )
+}}
+
+    
+select _id, company_id, invoice_id, total_units, status
+from `mongo_savia_core_qa.order`
+{% if is_incremental() %}
+    where updated_at > (select max(updated_at) from {{ this }})
+{% endif %}
+
