367fd5b0f14d605be63eef5d393da9a48eead8d2
Juan E Daza <jdazap@choucairtesting.com>
2024-02-17 14:35:39 -0500

    Single Company Model

---
+++ models/single_company_model.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        materialized="incremental",
+        unique_key="_id",
+    )
+}}
+
+    
+select *
+from {{source('mongo_savia_core_qa', 'order')}}
+where company_id = "629ec6e7541abf7b407b0ade"
+{% if is_incremental() %}
+    where updated_at > (select max(updated_at) from {{ this }})
+{% endif %}
