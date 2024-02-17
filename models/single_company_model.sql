857ba7198d9b7fc88ab09801044626d2fa606b9d
Juan E Daza <jdazap@choucairtesting.com>
2024-02-17 15:09:05 -0500

    Corrected SQL model

--- models/single_company_model.sql
+++ models/single_company_model.sql
@@ -3,12 +3,11 @@
         materialized="incremental",
         unique_key="_id",
     )
-}}
-
-    
-select *
+}}    
+select
+            *
 from {{source('mongo_savia_core_qa', 'order')}}
 where company_id = "629ec6e7541abf7b407b0ade"
 {% if is_incremental() %}
-    where updated_at > (select max(updated_at) from {{ this }})
+    AND updated_at > (select max(updated_at) from {{ this }})
 {% endif %}
