aa3c591df28fbfb6ac15b8799720eafe03309415
Mega61 <megauploads61@gmail.com>
2024-02-25 18:57:04 -0500

    Modified column names and positioning for PRD POC model

--- models/single_company_prod_poc.sql
+++ models/single_company_prod_poc.sql
@@ -21,10 +21,28 @@ products as (
 )
 
 select
-    oi._id,
+    oi._id AS order_item_id,
+    REPLACE(JSON_EXTRACT(o.customer, '$.fullName'), '"','') AS customer_full_name,
+    REPLACE(JSON_EXTRACT(o.customer, '$.idType'), '"','') AS customer_id_type,
+    REPLACE(JSON_EXTRACT(o.customer, '$.identification'), '"','') AS customer_identification,
     o.order_number,
-    p.name,
-    oi.quantity,
+    o.created_at,
+    o.total_before_tax AS order_total_before_tax,
+    o.total_after_tax AS order_total_after_tax,
+    o.total_units AS order_total_units,
+    p.name AS product_name,
+    oi.quantity AS order_item_quantity,
+    oi.price_before_tax AS order_item_price_before_tax,
+    oi.price_after_tax AS order_item_price_after_tax,
+    oi.tax AS order_item_tax,
+    p.sku,
+    p.active AS product_active,
+    p.price_before_tax AS product_price_before_tax,
+    p.price_after_tax AS product_price_after_tax,
+    REPLACE(JSON_EXTRACT(o.status, '$.description'), '"','') AS status_description,
+    REPLACE(JSON_EXTRACT(o.channel, '$.description'), '"','') AS channel_description,
+    o.promised_delivery_date_time,
+    o.order_number_shopify,
     o.updated_at
 from orders o
 inner join
