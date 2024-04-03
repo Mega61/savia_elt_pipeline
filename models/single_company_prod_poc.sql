9772a6aa489dfb001ee9b886113faa9becc7525d
Juan E Daza <jdazap@choucairtesting.com>
2024-04-02 22:36:21 -0500

    Added discount, net value and net discount. Changed all timestamps to UTC -5. Changed the incremental identifier to OrderItem Updated at rather than Order Updated at

--- models/single_company_prod_poc.sql
+++ models/single_company_prod_poc.sql
@@ -6,12 +6,19 @@
 }}
 
 with orders as (
-    SELECT *
+    SELECT 
+        *,
+        TIMESTAMP_SUB(created_at, INTERVAL 5 HOUR) AS created_at_col,
+        TIMESTAMP_SUB(promised_delivery_date_time, INTERVAL 5 HOUR) AS promised_delivery_date_time_col
     FROM {{source('mongo_savia_core_qa', 'order')}}
 ),
 
 order_items as (
-    select *
+    select 
+        *,
+        (price_before_tax * quantity) - ((price_before_tax * quantity) * (discount/100)) AS net_value,
+        (price_before_tax * quantity) * (discount/100) as net_discount,
+        TIMESTAMP_SUB(updated_at, INTERVAL 5 HOUR) AS updated_at_col
     from {{source('mongo_savia_core_qa', 'order_item')}}
 ),
 
@@ -26,7 +33,7 @@ select
     REPLACE(JSON_EXTRACT(o.customer, '$.idType'), '"','') AS customer_id_type,
     REPLACE(JSON_EXTRACT(o.customer, '$.identification'), '"','') AS customer_identification,
     o.order_number,
-    o.created_at,
+    o.created_at_col,
     o.total_before_tax AS order_total_before_tax,
     o.total_after_tax AS order_total_after_tax,
     o.total_units AS order_total_units,
@@ -35,15 +42,18 @@ select
     oi.price_before_tax AS order_item_price_before_tax,
     oi.price_after_tax AS order_item_price_after_tax,
     oi.tax AS order_item_tax,
+    oi.discount as order_item_discount,
+    oi.net_value,
+    oi.net_discount,
     p.sku,
     p.active AS product_active,
     p.price_before_tax AS product_price_before_tax,
     p.price_after_tax AS product_price_after_tax,
     REPLACE(JSON_EXTRACT(o.status, '$.description'), '"','') AS status_description,
     REPLACE(JSON_EXTRACT(o.channel, '$.description'), '"','') AS channel_description,
-    o.promised_delivery_date_time,
+    o.promised_delivery_date_time_col,
     o.order_number_shopify,
-    o.updated_at
+    oi.updated_at_col
 from orders o
 inner join
 order_items oi ON o._id = oi.order_id
@@ -52,5 +62,5 @@ products p ON oi.product_id = p._id
 where
 o.company_id = "629ec6e7541abf7b407b0ade"
 {% if is_incremental() %}
-    AND o.updated_at > (select max(updated_at) from {{ this }})
+    AND oi.updated_at_col > (select max(updated_at) from {{ this }})
 {% endif %}
